83fa35e26d93a0d4a8b6c8eb2feabb27ca5a0b42
ptyagicodecamp <ptyagicodecamp@gmail.com>
2017-07-10 14:51:44 -0700

    added sample kotlin activity

---
+++ app/src/main/java/org/pcc/kotlinandroid/samples/KotlinActivity.kt
@@ -0,0 +1,13 @@
+package org.pcc.kotlinandroid.samples
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.pcc.kotlinandroid.R
+
+class KotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+    }
+}
